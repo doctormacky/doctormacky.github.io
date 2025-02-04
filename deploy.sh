a9fdbbd6173694f174040b79f4eeb210ede53d0e
Doctormacky <liuyun105@126.com>
2025-02-04 21:55:49 +0800

    modify deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -23,14 +23,14 @@ git push -f $githubUrl master:gh-pages # 推送到github
 # deploy to coding
 
 #echo 'www.xugaoyi.com\nxugaoyi.com' > CNAME  # 自定义域名
-#if [ -z "$CODING_TOKEN" ]; then  # -z 字符串 长度为0则为true；$CODING_TOKEN来自于github仓库`Settings/Secrets`设置的私密环境变量
-#  codingUrl=git@git.dev.tencent.com:xugaoyi/xugaoyi.git
-#else
-#  codingUrl=https://xugaoyi:${CODING_TOKEN}@git.dev.tencent.com/xugaoyi/xugaoyi.git
-#fi
-#git add -A
-#git commit -m "${msg}"
-#git push -f $codingUrl master # 推送到coding
+if [ -z "$CODING_TOKEN" ]; then  # -z 字符串 长度为0则为true；$CODING_TOKEN来自于github仓库`Settings/Secrets`设置的私密环境变量
+  codingUrl=git@e.coding.net:g-mzfe7173/doctormacky/doctormacky.git
+else
+  codingUrl=https://pt6t91qgyk2v:${CODING_TOKEN}@e.coding.net:g-mzfe7173/doctormacky/doctormacky.git
+fi
+git add -A
+git commit -m "${msg}"
+git push -f $codingUrl master # 推送到coding
 
 cd -
 rm -rf docs/.vuepress/dist
